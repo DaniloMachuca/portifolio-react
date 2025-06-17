cccd6c762bfaa43311c3832e8fc4f0a477e877a5
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-06-17 07:25:55 -0300

    add: seção de projetos, componente de projetos

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,14 @@
+import Title from "../Title";
+import Text from "../Text";
+
+import { Card, BtnLink } from "./styles";
+
+const Project = () => (
+  <Card>
+    <Title>Projeto Lista de Tarefas</Title>
+    <Text color="secondary">Lista de tarefas feita com VueJs</Text>
+    <BtnLink>Visualizar</BtnLink>
+  </Card>
+);
+
+export default Project;
